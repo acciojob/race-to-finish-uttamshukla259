d2dee7929d245e3947c74a846713af066d0a4347
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-04-11 08:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 window.promises = [];
+for (let i = 0; i < 5; i++) {
+  const promise = new Promise((resolve) => {
+    const randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => resolve(randomTime), randomTime * 1000);
+  });
+  promises.push(promise);
+}
 
+Promise.any(promises).then((result) => {
+  const outputDiv = document.getElementById('output');
+  outputDiv.textContent = `The first promise to resolve took ${result} seconds.`;
+});
 // Do not change the code above this
 // add your promises to the array `promises`
